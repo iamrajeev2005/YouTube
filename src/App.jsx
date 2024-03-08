6bb3b3cf6effabb98010cad26bac37c03676b1e4
iamrajeev2005 <fea.rajeev@gmail.com>
2024-03-08 12:51:17 +0530

    started header made logo and menu icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./components/Header";
 function App() {
-  return <>
-  
-  </>
+  return (
+    <div className="bg-[#0F0F0F] min-h-screen w-screen text-white">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
